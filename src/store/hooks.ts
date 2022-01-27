44550ed1496bb20052751bf1e9b90aa4a47a8ccc
hamza.mahmood@funavry.com-at-890242958078 <hamza.mahmood@funavry.com-at-890242958078>
2022-01-27 22:24:17 +0500

    Changes

--- src/store/hooks.ts
+++
@@ -1,7 +0,0 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-import type { IRootState, IAppDispatch } from './redux';
-
-// https://redux-toolkit.js.org/tutorials/typescript#define-root-state-and-dispatch-types
-// Use throughout your app instead of plain `useDispatch` and `useSelector`
-export const useAppDispatch = () => useDispatch<IAppDispatch>();
-export const useAppSelector: TypedUseSelectorHook<IRootState> = useSelector;
